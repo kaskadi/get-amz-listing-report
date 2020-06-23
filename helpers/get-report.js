847189c02b190ff1db90b4ad16359d01a2b63949
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-23 13:48:28 +0200

    Added MWS initialization

--- helpers/get-report.js
+++ helpers/get-report.js
@@ -1,3 +1,8 @@
 module.exports = async (reportId) => {
-  
+  const MWS = require('mws-client')({
+    AWSAccessKeyId: process.env.ACCESS_KEY,
+    SellerId: process.env.SELLER_ID,
+    MWSAuthToken: process.env.MWS_AUTH_TOKEN,
+    parserType: 'text'
+  })
 }
