9bd26e1ce8e2893ec5dfa4fbf15cbab263ebe372
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-25 13:15:46 +0200

    Updated namings + added duplicates removal

--- helpers/process-report.js
+++ helpers/process-report.js
@@ -7,57 +7,26 @@ module.exports = (parsedReport, marketplace) => {
 
 function getIdsMap (parsedReport, idType) {
   const listings = idType === 'ean' ? parsedReport.filter(listing => listing['product-id-type'] === '4') : parsedReport
-  const idKeysMaps = getIdKeysMaps(idType)
-  const idDuplets = listings.map(listing => Object.fromEntries(idKeysMaps.map(idKeysMap => [idKeysMap.key, listing[idKeysMap.listingKey]])))
-  const ids = [...new Set(listings.map(listing => listing[idKeysMaps[0].listingKey]))]
-  return ids.map(buildIdsMap(idDuplets, idKeysMaps))
+  const keyMaps = getKeyMaps(idType)
+  const idDuplets = listings.map(listing => Object.fromEntries(keyMaps.map(keyMap => [keyMap.key, listing[keyMap.listingKey]])))
+  const ids = [...new Set(listings.map(listing => listing[keyMaps[0].listingKey]))]
+  return ids.map(buildIdsMap(idDuplets, keyMaps))
 }
 
-function getIdKeysMaps (idType) {
-  const IdKeysMaps = [{ key: 'ean', listingKey: 'product-id' }, { key: 'asin', listingKey: 'asin1' }, { key: 'sellerSku', listingKey: 'seller-sku' }]
-  const idIndex = IdKeysMaps.findIndex(IdKeysMap => IdKeysMap.key === idType)
-  return IdKeysMaps.slice(idIndex, idIndex + 2)
+function getKeyMaps (idType) {
+  const keyMaps = [{ key: 'ean', listingKey: 'product-id' }, { key: 'asin', listingKey: 'asin1' }, { key: 'sellerSku', listingKey: 'seller-sku' }]
+  const idIndex = keyMaps.findIndex(keyMap => keyMap.key === idType)
+  return keyMaps.slice(idIndex, idIndex + 2)
 }
 
-function buildIdsMap (idDuplets, idKeysMaps) {
+function buildIdsMap (idDuplets, keyMaps) {
   return id => {
-    const matchingDuplets = idDuplets.filter(duplet => duplet[idKeysMaps[0].key] === id)
-    const idMapHandler = (idKeysMap, i) => i === 0 ? [idKeysMap.key, id] : [`${idKeysMap.key}s`, matchingDuplets.map(duplet => duplet[idKeysMaps[1].key])]
-    const idMap = idKeysMaps.map(idMapHandler)
+    const matchingDuplets = idDuplets.filter(duplet => duplet[keyMaps[0].key] === id)
+    const secondaryIds = [...new Set(matchingDuplets.map(duplet => duplet[keyMaps[1].key]))]
+    const idMap = [
+      [keyMaps[0].key, id],
+      [`${keyMaps[1].key}s`, secondaryIds]
+    ]
     return Object.fromEntries(idMap)
   }
 }
-
-// module.exports = (parsedReport, marketplace) => {
-//   const eans = [...new Set(parsedReport.map(listing => listing['product-id']))]
-//   return {
-//     marketplace,
-//     stockData: eans.map(getDataPerEan(parsedReport))
-//   }
-// }
-
-// function getDataPerEan(parsedReport) {
-//   return ean => {
-//     const matchingListings = parsedReport.filter(getMatchingListings(ean))
-//     return {
-//       ean,
-//       stocks: matchingListings.map(getStocksForEntry)
-//     }
-//   }
-// }
-
-// function getMatchingListings(ean) {
-//   return listing => listing['product-id'] === ean
-// }
-
-// function getStocksForEntry(listing) {
-//   return {
-//     asins: Object.entries(listing).filter(entry => entry[0].includes('asin')).map(entry => entry[1]).filter(asin => asin.length > 0),
-//     sellerSku: listing['seller-sku'],
-//     quantity: listing.quantity,
-//     pendingQuantity: listing['pending-quantity'],
-//     fulfilmentChannel: listing['fulfilment-channel'],
-//     status: listing.status,
-//     condition: listing['item-condition']
-//   }
-// }
