a5599e00c6fb057529681fae77d827e554ec7e49
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-25 12:56:31 +0200

    Added raw report trimming to fix line mapping

--- helpers/parse-report.js
+++ helpers/parse-report.js
@@ -1,5 +1,6 @@
 module.exports = (rawReport) => {
-  const lines = rawReport.split('\n')
+  const report = rawReport.trim()
+  const lines = report.split('\n')
   const columns = lines[0].split('\t')
   return lines.slice(1).map(line => Object.fromEntries(line.split('\t').map((value, i) => [columns[i], value])))
 }
