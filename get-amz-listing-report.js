27d7785e8cb56c1bf6647cc45c45ffda2847e3ab
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-26 12:49:19 +0200

    Aligned naming with repo

--- get-amz-listing-report.js
+++ get-amz-listing-report.js
@@ -3,6 +3,8 @@ module.exports.handler = async (event) => {
   const getReport = require('./helpers/get-report.js')
   const parseReport = require('./helpers/parse-report.js')
   const processReport = require('./helpers/process-report.js')
+  console.log(JSON.stringify(event, null, 2))
+  return true
   const msgData = processMsg(event.Records[0]) // batchSize set to 1 so we only receive 1 message at a time, hence message = event.Records[0]
   if (!msgData.reportId) {
     return {}
