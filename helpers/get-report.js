09f36b990229d1907df7b627799c1d8709ca726d
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-10 16:20:34 +0200

    Hotfix: updated environment variables

--- helpers/get-report.js
+++ helpers/get-report.js
@@ -1,8 +1,8 @@
 module.exports = async (msgData) => {
   const MWS = require('mws-client')({
-    AWSAccessKeyId: process.env.ACCESS_KEY,
-    SellerId: process.env.SELLER_ID,
-    MWSAuthToken: process.env.MWS_AUTH_TOKEN,
+    AWSAccessKeyId: process.env.MWS_KEY_ID,
+    SellerId: process.env.AMZ_EU_SELLER_ID,
+    MWSAuthToken: process.env.MWS_KEY_SECRET,
     parserType: 'text'
   })
   const mwsData = await MWS.reports.getReport({
--- helpers/process-report/get-quantity-data.js
+++ helpers/process-report/get-quantity-data.js
@@ -1,7 +1,7 @@
 const MWS = require('mws-client')({
-  AWSAccessKeyId: process.env.ACCESS_KEY,
-  SellerId: process.env.SELLER_ID,
-  MWSAuthToken: process.env.MWS_AUTH_TOKEN
+  AWSAccessKeyId: process.env.MWS_KEY_ID,
+  SellerId: process.env.AMZ_EU_SELLER_ID,
+  MWSAuthToken: process.env.MWS_KEY_SECRET
 })
 
 module.exports = async (productIdsMaps, marketplace) => {
