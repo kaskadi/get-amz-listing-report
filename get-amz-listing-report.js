43c11447d837143a19571e67d58e27075cb26c30
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-09 13:50:41 +0200

    Changed trigger to SQS + publish to SQS

--- get-amz-listing-report.js
+++ get-amz-listing-report.js
@@ -3,8 +3,10 @@ module.exports.handler = async (event) => {
   const getReport = require('./helpers/get-report.js')
   const parseReport = require('./helpers/parse-report.js')
   const processReport = require('./helpers/process-report/process-report.js')
+  const publishMsg = require('./helpers/publish-msg.js')
   const msgData = processMsg(JSON.parse(event.Records[0].Sns.Message)) // batchSize set to 1 so we only receive 1 message at a time, hence message = event.Records[0]
   return await getReport(msgData)
   .then(parseReport)
   .then(processReport(msgData.marketplace))
+  .then(data => publishMsg(data, process.env.QUEUE_URL))
 }
