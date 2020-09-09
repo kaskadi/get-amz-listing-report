fad60b03f475cb7983cfd06b3ba627d2bac6c7fa
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-09 14:08:05 +0200

    Temporarily removed logic

--- get-amz-listing-report.js
+++ get-amz-listing-report.js
@@ -4,9 +4,11 @@ module.exports.handler = async (event) => {
   const parseReport = require('./helpers/parse-report.js')
   const processReport = require('./helpers/process-report/process-report.js')
   const publishMsg = require('./helpers/publish-msg.js')
-  const msgData = processMsg(JSON.parse(event.Records[0].Sns.Message)) // batchSize set to 1 so we only receive 1 message at a time, hence message = event.Records[0]
-  return await getReport(msgData)
-  .then(parseReport)
-  .then(processReport(msgData.marketplace))
-  .then(data => publishMsg(data, process.env.QUEUE_URL))
+  console.log(JSON.stringify(event, null, 2))
+  return true
+  // const msgData = processMsg(JSON.parse(event.Records[0].Sns.Message)) // batchSize set to 1 so we only receive 1 message at a time, hence message = event.Records[0]
+  // return await getReport(msgData)
+  // .then(parseReport)
+  // .then(processReport(msgData.marketplace))
+  // .then(data => publishMsg(data, process.env.QUEUE_URL))
 }
