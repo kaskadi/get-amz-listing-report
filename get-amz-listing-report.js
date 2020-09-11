80c2a7f8c178f2f7e3679ffa96d67d1df3ef0d63
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-11 12:10:31 +0200

    Implemented new lambda logic with report polling

--- get-amz-listing-report.js
+++ get-amz-listing-report.js
@@ -1,17 +1,18 @@
 module.exports.handler = async (event) => {
-  const processMsg = require('./helpers/process-msg.js')
   const getReport = require('./helpers/get-report.js')
   const parseReport = require('./helpers/parse-report.js')
   const processReport = require('./helpers/process-report/process-report.js')
-  const publishMsg = require('./helpers/publish-msg.js')
   console.log(JSON.stringify(event, null, 2))
+  const eventPayload = event.detail.responsePayload
+  let stocks = []
+  for (const marketplace of eventPayload.marketplaces) {
+    stocksData = await getReport(eventPayload.ReportRequestInfo.ReportRequestId, marketplace)
+      .then(parseReport)
+      .then(processReport(marketplace))
+    stocks = [...stocks, stocksData]
+  }
   return {
-    stocks: [],
+    stocks,
     stocksEventType: 'amz-stocks-update'
   }
-  // const msgData = processMsg(JSON.parse(event.Records[0].Sns.Message)) // batchSize set to 1 so we only receive 1 message at a time, hence message = event.Records[0]
-  // return await getReport(msgData)
-  // .then(parseReport)
-  // .then(processReport(msgData.marketplace))
-  // .then(data => publishMsg(data, process.env.QUEUE_URL))
 }
--- helpers/get-report.js
+++ helpers/get-report.js
@@ -1,14 +1,44 @@
-module.exports = async (msgData) => {
-  const MWS = require('mws-client')({
-    AWSAccessKeyId: process.env.MWS_KEY_ID,
-    SellerId: process.env.AMZ_EU_SELLER_ID,
-    MWSAuthToken: process.env.MWS_KEY_SECRET,
-    parserType: 'text'
-  })
-  const mwsData = await MWS.reports.getReport({
-    _marketplace: msgData.marketplace,
+const MWS = require('mws-client')({
+  AWSAccessKeyId: process.env.MWS_KEY_ID,
+  SellerId: process.env.AMZ_EU_SELLER_ID,
+  MWSAuthToken: process.env.MWS_KEY_SECRET
+})
+
+module.exports = (reportRequestId, marketplace) => {
+  return pollReport(reportRequestId, marketplace)
+    .then(getReportId(marketplace))
+    .then(fetchReport(marketplace))
+}
+
+async function pollReport (reportRequestId, marketplace) {
+  let reportReady = false
+  let reportRequestInfo
+  while (!reportReady) {
+    reportRequestInfo = await MWS.reports.getReportRequestList({
+      _marketplace: marketplace,
+      _httpMethod: 'POST',
+      'ReportRequestIdList.Id.1': reportRequestId
+    }).then(data => data.body.GetReportRequestListResponse.GetReportRequestListResult.ReportRequestInfo)
+    reportReady = reportRequestInfo.ReportProcessingStatus === '_DONE_'
+    if (!reportReady) {
+      await new Promise(resolve => setTimeout(resolve, 45000))
+    }
+  }
+  return reportRequestInfo
+}
+
+function getReportId (marketplace) {
+  return reportRequestInfo => reportRequestInfo.GeneratedReportId || MWS.reports.getReportList({
+    _marketplace: marketplace,
     _httpMethod: 'POST',
-    ReportId: msgData.reportId
-  })
-  return mwsData.body
+    'ReportRequestIdList.Id.1': reportRequestId
+  }).then(data => data.body.GetReportListResponse.GetReportListResult.ReportInfo.ReportId)
+}
+
+function fetchReport (marketplace) {
+  return reportId => MWS.reports.getReport({
+    _marketplace: marketplace,
+    _httpMethod: 'POST',
+    ReportId: reportId
+  }).then(data => data.body)
 }
